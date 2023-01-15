9d378cf9f6c11d0bfeada66aa97f8c05f69f746a
Jarnoldi <jarnoldinho@gmail.com>
2023-01-15 18:51:19 +0200

    first commit

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,10 @@
+import spinneri from '../layout/assets/spinneri.gif'
+
+function Spinner() {
+  return (
+    <div className='w-100 mt-200'>
+        <img width={180} className='¨text-center mx-auto' src={spinneri} alt="Loading..." />
+    </div>
+  )
+}
+export default Spinner
